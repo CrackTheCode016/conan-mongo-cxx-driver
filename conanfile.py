e8edeb1eced34c58a81aca4da1e90fce5aa26c59
CrackTheCode016 <ibnbassem@gmail.com>
2020-06-14 09:04:47 -0400

    updated to 3.4.0

--- conanfile.py
+++ conanfile.py
@@ -3,25 +3,28 @@ import os
 import shutil
 import re
 
+
 class MongoCxxConan(ConanFile):
     name = "mongo-cxx-driver"
-    version = "3.3.0"
+    version = "3.4.0"
     url = "http://github.com/bincrafters/conan-mongo-cxx-driver"
     description = "C++ Driver for MongoDB"
-    license = "https://github.com/mongodb/mongo-cxx-driver/blob/{0}/LICENSE".format(version)
-    settings =  "os", "compiler", "arch", "build_type"
+    license = "https://github.com/mongodb/mongo-cxx-driver/blob/{0}/LICENSE".format(
+        version)
+    settings = "os", "compiler", "arch", "build_type"
     options = {"shared": [True, False]}
     default_options = {'shared': 'False'}
-    requires = 'mongo-c-driver/[~=1.9]@bincrafters/stable'
+    requires = 'mongo-c-driver/[1.16.1]@bincrafters/stable'
     generators = "cmake"
 
     def source(self):
-        tools.get("https://github.com/mongodb/mongo-cxx-driver/archive/r{0}.tar.gz".format(self.version))
+        tools.get(
+            "https://github.com/mongodb/mongo-cxx-driver/archive/r{0}.tar.gz".format(self.version))
         extracted_dir = "mongo-cxx-driver-r{0}".format(self.version)
         os.rename(extracted_dir, "sources")
 
     def build(self):
-        conan_magic_lines='''project(MONGO_CXX_DRIVER LANGUAGES CXX)
+        conan_magic_lines = '''project(MONGO_CXX_DRIVER LANGUAGES CXX)
         include(../conanbuildinfo.cmake)
         conan_basic_setup()
         # include(CheckCXXCompilerFlag)
@@ -53,14 +56,16 @@ class MongoCxxConan(ConanFile):
         set(BSONCXX_POLY_USE_BOOST 0)
         set(BSONCXX_POLY_USE_STD 0)
         '''
-        
+
         cmake_file = "sources/CMakeLists.txt"
-        tools.replace_in_file(cmake_file, "project(MONGO_CXX_DRIVER LANGUAGES CXX)", conan_magic_lines)
+        tools.replace_in_file(
+            cmake_file, "project(MONGO_CXX_DRIVER LANGUAGES CXX)", conan_magic_lines)
         content = tools.load(cmake_file)
 
         cmake = CMake(self)
         if self.settings.compiler == 'Visual Studio':
-            cmake.definitions["BSONCXX_POLY_USE_BOOST"] = 1 # required for Windows.
+            # required for Windows.
+            cmake.definitions["BSONCXX_POLY_USE_BOOST"] = 1
         cmake.configure(source_dir="sources")
         cmake.build()
 
@@ -72,11 +77,16 @@ class MongoCxxConan(ConanFile):
 
     def package(self):
         self.copy(pattern="COPYING*", src="sources")
-        self.copy(pattern="*.hpp", dst="include/bsoncxx", src="sources/src/bsoncxx", keep_path=True)
-        self.copy(pattern="*.hpp", dst="include/mongocxx", src="sources/src/mongocxx", keep_path=True)
-        self.copy(pattern="*.hpp", dst="include/bsoncxx", src="src/bsoncxx", keep_path=True)
-        self.copy(pattern="*.hpp", dst="include/mongocxx", src="src/mongocxx", keep_path=True)
-        self.copy(pattern="*.hpp", dst="include/bsoncxx/third_party/mnmlstc/core", src="src/bsoncxx/third_party/EP_mnmlstc_core-prefix/src/EP_mnmlstc_core/include/core", keep_path=False)
+        self.copy(pattern="*.hpp", dst="include/bsoncxx",
+                  src="sources/src/bsoncxx", keep_path=True)
+        self.copy(pattern="*.hpp", dst="include/mongocxx",
+                  src="sources/src/mongocxx", keep_path=True)
+        self.copy(pattern="*.hpp", dst="include/bsoncxx",
+                  src="src/bsoncxx", keep_path=True)
+        self.copy(pattern="*.hpp", dst="include/mongocxx",
+                  src="src/mongocxx", keep_path=True)
+        self.copy(pattern="*.hpp", dst="include/bsoncxx/third_party/mnmlstc/core",
+                  src="src/bsoncxx/third_party/EP_mnmlstc_core-prefix/src/EP_mnmlstc_core/include/core", keep_path=False)
         # self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
 
         # self.purge("lib", "lib.*testing.*".format(self.version))
@@ -102,11 +112,11 @@ class MongoCxxConan(ConanFile):
         self.copy(pattern="lib*cxx.lib", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="lib*cxx.a", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="lib*cxx.so*", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="lib*cxx.dylib", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="lib*cxx._noabi.dylib", dst="lib", src="lib", keep_path=False)
+        self.copy(pattern="lib*cxx.dylib", dst="lib",
+                  src="lib", keep_path=False)
+        self.copy(pattern="lib*cxx._noabi.dylib",
+                  dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ['mongocxx', 'bsoncxx']
         self.cpp_info.includedirs.append('include/bsoncxx/third_party/mnmlstc')
-
-
